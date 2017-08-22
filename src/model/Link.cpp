3b3f226ad83b9d2df74b4a404b4c79afe8ac1dbb
a1994846931931 <a1994846931931@foxmail.com>
2017-08-22 14:28:24 +0800

    Link conflict fixed

--- src/model/Link.cpp
+++ src/model/Link.cpp
@@ -19,19 +19,8 @@ namespace model {
 
 Frame* Link::getFrame()
 {
-<<<<<<< Updated upstream
 	 return _frame;
 }
-=======
-	_theta=theta;
-	_d=d;
-	_a=a;
-	_alpha=alpha;
-	_sigma=sigma;
-	//_offset=offset;
-	_lmin=min;
-	_lmax=max;
->>>>>>> Stashed changes
 
 Link::Link(double alpha, double a, double d, double theta, double min, double max, bool sigma):
 		_theta(theta),_d(d),_a(a),_alpha(alpha),
@@ -126,17 +115,9 @@ const DHParameters& Link::getDHParams() const
 	return _dHParam;
 }
 
-<<<<<<< Updated upstream
-Link::~Link() {
-<<<<<<< HEAD
-
-=======
 Link::~Link()
 {
->>>>>>> Stashed changes
-=======
 	delete _frame;
->>>>>>> pieper
 }
 
 } /* namespace model */
