15cc1093fd5e1baccee9f1cbc1b01b8e1f20e19f
a1994846931931 <a1994846931931@foxmail.com>
2017-09-11 10:37:14 +0800

    添加CompositeInterpolator注释

--- src/trajectory/CompositeInterpolator.h
+++ src/trajectory/CompositeInterpolator.h
@@ -1,8 +1,7 @@
-/*
- * CompositeInterpolator.h
- *
- *  Created on: Sep 8, 2017
- *      Author: a1994846931931
+/**
+ * @brief CompositeInterpolator类, LinearCompositeInterpolator类
+ * @date Sep 8, 2017
+ * @author a1994846931931
  */
 
 #ifndef COMPOSITEINTERPOLATOR_H_
@@ -22,12 +21,22 @@ namespace trajectory {
 /**
  * @brief 复合插补器
  *
- * 用于一个主插补器和一个double类型的映射插补器的复合.
- *
+ * 用于一个主插补器@f$ f(t)@f$ 和一个double类型的映射插补器@f$ e(t) @f$ 的复合. 复合插补器的插补时长变为e(t)的插补时长.
+ * - 复合后的插补器函数为:
+ * 	- @f$ f(e(t)) @f$ .
+ * - 复合插补器的一次导为:
+ * 	- @f$ \frac {df(e(t))}{dt} = \dot f(e(t)) \cdot\dot e(t)@f$;
+ * - 二次导为:
+ * 	- @f$ \frac {d^2f(e(t))}{dt^2} = {f(e(t))}^{(2)} \cdot {\dot e(t)}^2 + \dot f(e(t))\cdot {e(t)}^{(2)}@f$
  */
 template <class T>
 class CompositeInterpolator: public Interpolator<T> {
 public:
+	/**
+	 * @brief 构造函数
+	 * @param interpolator [in] 主插补器
+	 * @param mapper [in] 映射插补器
+	 */
 	CompositeInterpolator(Interpolator<T>* interpolator, Interpolator<double>* mapper)
 	{
 		_interpolator = interpolator;
@@ -57,13 +66,36 @@ public:
 	}
 	virtual ~CompositeInterpolator(){}
 private:
+	/**
+	 * @brief 主插补器地址
+	 */
 	Interpolator<T>* _interpolator;
+
+	/**
+	 * @brief 映射器地址
+	 */
 	Interpolator<double>* _mapper;
 };
 
+/**
+ * @brief 映射器@f$ e(t) = k\cdot t @f$ 的复合插补器
+ *
+ * @f$ k为系数@f$ @f$ e(t) = k\cdot t @f$
+ * - 复合后的插补器函数为:
+ * 	- @f$ f(k\cdot t)) @f$ .
+ * - 复合插补器的一次导为:
+ * 	- @f$ \frac {df(k\cdot t)}{dt} = k\cdot f(k\cdot t))@f$;
+ * - 二次导为:
+ * 	- @f$ \frac {d^2f(k\cdot t)}{dt^2} = k^2 {f(k\cdot t)}^{(2)}@f$
+ */
 template <class T>
 class LinearCompositeInterpolator: public Interpolator<T> {
 public:
+	/**
+	 * @brief 构造函数
+	 * @param interpolator [in] 主插补器
+	 * @param factor [in] 系数
+	 */
 	LinearCompositeInterpolator(Interpolator<T>* interpolator, double factor)
 	{
 		_interpolator = interpolator;
