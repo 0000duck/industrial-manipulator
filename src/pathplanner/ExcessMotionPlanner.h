4a0812e455e3ab00795e2c26ab8f0533d14204fa
Zhu Ruifeng <13588821896@163.com>
2017-09-18 11:20:05 +0800

    Excess

---
+++ src/pathplanner/ExcessMotionPlanner.h
@@ -0,0 +1,17 @@
+/*
+ * ExcessMotionPlanner.h
+ *
+ *  Created on: Sep 18, 2017
+ *      Author: zrf
+ */
+
+#ifndef EXCESSMOTIONPLANNER_H_
+#define EXCESSMOTIONPLANNER_H_
+
+class ExcessMotionPlanner {
+public:
+	ExcessMotionPlanner();
+	virtual ~ExcessMotionPlanner();
+};
+
+#endif /* EXCESSMOTIONPLANNER_H_ */
