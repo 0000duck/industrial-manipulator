d8f6750224348015f4bbd5d59d9c0d86ed98d413
a1994846931931 <a1994846931931@foxmail.com>
2017-09-26 13:14:17 +0800

    圆弧混和多线段规划器

---
+++ src/pathplanner/MultiLineArcBlendPlanner.cpp
@@ -0,0 +1,23 @@
+/*
+ * MultiLineArcBlendPlanner.cpp
+ *
+ *  Created on: Sep 26, 2017
+ *      Author: a1994846931931
+ */
+
+#include "MultiLineArcBlendPlanner.h"
+
+namespace robot {
+namespace pathplanner {
+
+MultiLineArcBlendPlanner::MultiLineArcBlendPlanner() {
+	// TODO Auto-generated constructor stub
+
+}
+
+MultiLineArcBlendPlanner::~MultiLineArcBlendPlanner() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace pathplanner */
+} /* namespace robot */
---
+++ src/pathplanner/MultiLineArcBlendPlanner.h
@@ -0,0 +1,23 @@
+/*
+ * MultiLineArcBlendPlanner.h
+ *
+ *  Created on: Sep 26, 2017
+ *      Author: a1994846931931
+ */
+
+#ifndef MULTILINEARCBLENDPLANNER_H_
+#define MULTILINEARCBLENDPLANNER_H_
+
+namespace robot {
+namespace pathplanner {
+
+class MultiLineArcBlendPlanner {
+public:
+	MultiLineArcBlendPlanner();
+	virtual ~MultiLineArcBlendPlanner();
+};
+
+} /* namespace pathplanner */
+} /* namespace robot */
+
+#endif /* MULTILINEARCBLENDPLANNER_H_ */
