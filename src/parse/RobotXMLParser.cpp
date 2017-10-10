1a05c0c6876a32cf7e9e8259bbaf7a9728419650
a1994846931931 <a1994846931931@foxmail.com>
2017-10-10 17:08:29 +0800

    empty model parser

---
+++ src/parse/RobotXMLParser.cpp
@@ -0,0 +1,23 @@
+/*
+ * RobotXMLParser.cpp
+ *
+ *  Created on: Oct 10, 2017
+ *      Author: a1994846931931
+ */
+
+#include "RobotXMLParser.h"
+
+namespace robot {
+namespace parse {
+
+RobotXMLParser::RobotXMLParser() {
+	// TODO Auto-generated constructor stub
+
+}
+
+RobotXMLParser::~RobotXMLParser() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace parse */
+} /* namespace robot */
---
+++ src/parse/RobotXMLParser.h
@@ -0,0 +1,23 @@
+/*
+ * RobotXMLParser.h
+ *
+ *  Created on: Oct 10, 2017
+ *      Author: a1994846931931
+ */
+
+#ifndef ROBOTXMLPARSER_H_
+#define ROBOTXMLPARSER_H_
+
+namespace robot {
+namespace parse {
+
+class RobotXMLParser {
+public:
+	RobotXMLParser();
+	virtual ~RobotXMLParser();
+};
+
+} /* namespace parse */
+} /* namespace robot */
+
+#endif /* ROBOTXMLPARSER_H_ */
