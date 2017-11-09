2ef352cbf0eb8f39ddf10eac35eead9a88e7a67a
a1994846931931 <a1994846931931@foxmail.com>
2017-11-09 09:00:41 +0800

    添加注释

--- src/pathplanner/TimeOptimalPlanner.h
+++ src/pathplanner/TimeOptimalPlanner.h
@@ -25,7 +25,7 @@ public:
 	static void optimizeVelocityRestriction(vector<double>& maxSpeed, double a, double h, double ds);
 
 	/**
-	 * @brief 时间最优s型规划
+	 * @brief 根据最大速度函数时间最优s型规划
 	 * @param Vm [in] 获取最大速度限制的函数
 	 * @param s [in] 规划的距离
 	 * @param ve [in] 期望速度
@@ -37,6 +37,17 @@ public:
 	 */
 	static SequenceInterpolator<double>::ptr getOptimalLt(std::function<double(double)>& Vm, double s, double ve, double a, double h, double ds);
 
+	/**
+	 * @brief 根据已优化的最大速度采样时间最优s型规划
+	 * @param optimizedMaxSpeed [in] 采样的最大速度,采样长度间隔为ds,且两个采样点之间的速度必然可以由
+	 * 一个二速度规划到达. 通常要由optimizeVelocityRestriction函数进行优化.
+	 * @param s [in] 规划的距离
+	 * @param ve [in] 期望速度
+	 * @param a [in] 使用加速度
+	 * @param h [in] 使用加加速度
+	 * @param ds [in] 沿轨迹长度采样精度
+	 * @return 时间最优的长度-时间插补器
+	 */
 	static SequenceInterpolator<double>::ptr getOptimalLt(vector<double> optimizedMaxSpeed, double s, double ve, double a, double h, double ds);
 };
 
